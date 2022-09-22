6229bbb02505196743f295bcd85bdf3c6984aaed
xuyi <540767457@qq.com>
2022-09-22 22:55:05 +0800

    加了消息类型，同时引入了一些包

---
+++ Ten_Thousand_Game_Server/Msg.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <memory>
+using namespace std;
+
+class BaseMsg
+{
+public:
+	enum TYPE {
+		SERVICE = 1,
+	};
+
+	int type;
+	char load[99999999]{};	//用于检测内存泄漏，仅用于调试
+	virtual ~BaseMsg();
+
+};
+
+
+class ServiceMsg :public BaseMsg {
+public:
+	int source; 
+	shared_ptr<char> buff;
+	size_t size;
+};
---
+++ Ten_Thousand_Game_Server/Service.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <queue>
+#include <thread>
+#include <pthread.h>
+#include "Msg.h"
+#include <string>
+
+using namespace std;
+
+class Service
+{
+public:
+	uint32_t id;
+	shared_ptr<string> type;
+	bool isExiting = false;
+
+	queue<shared_ptr<BaseMsg>> msgQueue;
+	pthread_spinlock_t queueLock;
+	
+	Service();
+	~Service();
+
+	void OnInit();
+	void OnMsg(shared_ptr<BaseMsg> msg);
+	void OnExit();
+
+	void PushMsg(shared_ptr<BaseMsg> msg);
+
+	bool ProcessMsg();
+	void ProcessMsgs(int max);
+
+private:
+	shared_ptr<BaseMsg> PopMsg();
+};
+
